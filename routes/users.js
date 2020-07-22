b497876820e4c069082281779b00fd8cc2e5a0d1
Andy Llactahuamani <andy.llactahuamani@ryerson.ca>
2020-07-22 09:05:30 -0400

    Create and Respond with JSON Webtoken

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -12,7 +15,7 @@ router.post('/', [
     check('name', 'Please add name').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
-], (req, res) => {
+], async (req, res) => {
     // in order to use req.body we need to add middleware to server.js
     // res.send(req.body);
     const errors = validationResult(req);
@@ -20,7 +23,47 @@ router.post('/', [
         return res.status(400).json({errors: errors.array()});
     } 
     
-    res.send('passed'); 
+    const {name, email, password} = req.body;
+
+    try {
+        let user = await User.findOne({email: email});
+
+        if(user){
+            return res.status(400).json({msg: 'User already exists'});
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        // res.send('User saved');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            // 3600 secs = 1 hour
+            expiresIn: 3600
+        }, (err, token) => {
+            if (err) throw err;
+            res.json({token});
+            
+        });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
 });
 
 // we have to export the router or it won't work
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /*
 npm i express bcryptjs jsonwebtoken config express-validator mongoose
 
-express - web framework to handle routing
+express - web framework to handle routing (Routing refers to how an app's endpoints (URIs) respond to client requests https://expressjs.com/en/guide/routing.html)
 bcryptjs - handles hashing passwords, b/c we have authentication in our app
 jsonwebtoken - for authentication, we can create JSON Web Token (JWT) which is used to access protected routes on the backend
 config - for global variables
 express-validator - validate any body of data coming in
-mongoose - an abstraction layer, so we can deal with out database. Also allowing us to create models
+mongoose - an abstraction layer, so we can deal with our database. Also allowing us to create models
 
 Development dependencies
 npm i -D nodemon concurrently
@@ -19,7 +19,7 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Connect Database
+// Connect Database, connecting to MongoDB is dependant on IP address so you can't really connect to the database while you're at work. Must be some issue caused by network routing at job
 connectDB();
 
 // Init Middleware
@@ -30,7 +30,7 @@ app.use(express.json({extended: false}));
 app.get('/', (req, res) => res.json({msg: 'Welcome to the Contact Keeper API...'}));
 
 // Define Routes
-// eg. anything that uses /api/users will be routed to users.js
+// eg. anything that uses /api/users,auth,contacts will be routed to users.js
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
